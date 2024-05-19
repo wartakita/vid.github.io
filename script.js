c1f02e8244d0c5e41872f05d15a8719c22b9f886
wartakita <161806668+wartakita@users.noreply.github.com>
2024-05-19 17:49:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-function getParameterByName(name, url = window.location.href) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
-}
+        function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const mediaPlayer = document.querySelector('media-player');
-    const src = getParameterByName('src');
-    if (src) {
-        mediaPlayer.setAttribute('src', src);
-    }
-});
+        function base64Decode(str) {
+            try {
+                return atob(str);
+            } catch (e) {
+                console.error('Failed to decode Base64 string', e);
+                return '';
+            }
+        }
+
+        document.addEventListener('DOMContentLoaded', () => {
+            const srcBase64 = getParameterByName('src');
+            const posterBase64 = getParameterByName('poster');
+
+            const src = srcBase64 ? base64Decode(srcBase64) : '';
+            const poster = posterBase64 ? base64Decode(posterBase64) : '';
+
+            const mediaPlayer = document.getElementById('mediaPlayer');
+            if (src) {
+                mediaPlayer.setAttribute('src', src);
+            }
+            if (poster) {
+                mediaPlayer.setAttribute('poster', poster);
+            }
+        });
