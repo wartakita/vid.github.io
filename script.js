e9b7bbaecac3432cf0aba16368cb5d6fa1ba9b64
wartakita <161806668+wartakita@users.noreply.github.com>
2024-05-19 17:38:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ function getParameterByName(name, url = window.location.href) {
         results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
-    var paramValue = decodeURIComponent(results[2].replace(/\+/g, ' '));
-    // Encode the parameter value with Base64
-    var base64EncodedValue = btoa(paramValue);
-    return base64EncodedValue;
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const mediaPlayer = document.querySelector('media-player');
+    const src = getParameterByName('src');
+    if (src) {
+        mediaPlayer.setAttribute('src', src);
+    }
+});
