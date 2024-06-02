e13a4e969b1e65522865ea5ef67f150c0aba38c8
wartakita <161806668+wartakita@users.noreply.github.com>
2024-06-02 17:39:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,19 +13,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (src) {
         mediaPlayer.setAttribute('src', src);
     }
-
-    // Fungsi untuk decode Base64
-function decodeBase64(str) {
-    return decodeURIComponent(escape(atob(str)));
-}
-
-// Mengambil parameter 'src' dari URL
-const srcParam = getParameterByName('src');
-
-// Jika parameter 'src' ada, decode dan setel sebagai atribut src dari iframe
-if (srcParam) {
-    const decodedSrc = decodeBase64(srcParam);
-    document.getElementById('main-iframe').src = decodedSrc;
-}
-
 });
